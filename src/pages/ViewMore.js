ddeacfcee80384d2833262b55e56f52d47f08923
abkazan <abkazan@wisc.edu>
2023-11-04 17:36:12 -0500

    style changes

--- src/pages/ViewMore.js
+++ src/pages/ViewMore.js
@@ -4,63 +4,65 @@ import styles from './viewmore.module.scss';
 import projectData from '../components/new_project_data.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
 const ViewMore = () => {
+
     const [loading, setLoading] = useState(true);
     const [project, setProject] = useState('');
+
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         const projectToView = urlParams.get('project');
-        /* console.log(`project to view: ${projectToView}`); */
         const index = projectData.findIndex(obj => obj.title === projectToView);
-        /* console.log(projectData[index]); */
-
         setProject(projectData[index]);
         setTimeout(() => {
             setLoading(false);
         }, 1000)
     }, []);
+
     const [goingBack, setGoingBack] = useState(false);
+
     const goBack = () => {
         setGoingBack(true);
         setTimeout(() => {
             window.history.back()
         }, 700);
     }
+
     return (
         <>
-        <button className={`${styles.backBtn} ${goingBack ? styles.goingBack : ''}`}
-        onClick={goBack}>
-            <FontAwesomeIcon icon={faArrowLeft} />
+            <button className={`${styles.backBtn} ${goingBack ? styles.goingBack : ''}`}
+                onClick={goBack}>
+                <FontAwesomeIcon icon={faArrowLeft} />
             </button>
+
             <div className={styles.page}>
                 {loading ? (
                     <>
+
                         <div className={styles.loading}>
                             <h1>Loading...</h1>
                             <Loader />
-
                         </div>
 
                     </>
                 ) : (
                     <>
-                        
+
                         <h1>{project.title}</h1>
 
                         <div className={styles.container}>
+
                             <div className={styles.descAndImg}>
                                 <p>{project.desc}</p>
                                 <img src={project.img} alt="project img" />
                             </div>
 
                             <div className={styles.techAndConcepts}>
+                                {Object.keys(project).map((key) => (
+                                    typeof project[key] === 'object' && (
+                                        <>
 
-                                
-
-                                    {Object.keys(project).map((key) => (
-
-                                        typeof project[key] === 'object' && (
-                                            <>
                                             <div className={styles.list}>
                                                 <h2>{key}</h2>
                                                 <ul>
@@ -71,33 +73,10 @@ const ViewMore = () => {
                                                     ))}
                                                 </ul>
                                             </div>
-                                            
-                                            </>
-                                        )
-                                    ))}
 
-                                
-
-                                {/*  <div className={styles.list}>
-                                    <h2>Tech Used:</h2>
-                                    <ul>
-                                        {project.tech.map((item) => (
-                                            <li><p>{item}</p></li>
-                                        ))}
-                                    </ul>
-                                </div>
-
-
-                                <div className={styles.list}>
-                                    <h2>Concepts Learned:</h2>
-                                    <ul>
-                                        {project.concepts.map((item) => (
-                                            <li><p>{item}</p></li>
-                                        ))}
-                                    </ul>
-                                </div> */}
-
-
+                                        </>
+                                    )
+                                ))}
                             </div>
                         </div>
 
@@ -109,4 +88,5 @@ const ViewMore = () => {
         </>
     );
 };
+
 export default ViewMore;
